9c59240d6f11fd0ca85a70417afad04d16933f1a
Mohammed Zeeshan Ahmed <moahmed@dhcp35-90.lab.eng.blr.redhat.com>
2016-02-25 16:58:19 +0530

    Prepared most of the skeleton for pulpadmin setup script.

--- registry/scripts/pulpadminsetup.sh
+++ registry/scripts/pulpadminsetup.sh
@@ -16,14 +16,22 @@ if (( $EUID != 0 )); then
 	exit 0;
 fi
 
-#GLOBALS - END USER ONLY NEEDS TO MOD THESE
+# GLOBALS
+
+## End user can mod these
 PULPSERVER="172.29.32.79";
 PULP_VERIFYSSL="false";
-REPO="rhel"; # other option is fedora
+### Keep one active
+REPO="rhel"; 
+#REPO="fedora"; 
+
+## The untouchables
+DONE="\x1b[32mDONE\x1b[0m"
+PULPADMIN="admin";
+PULPADMINPASS="cccp@devcloud";
 
 # Warn user to do his updates
 proceed="z";
-
 while true; do
 	echo;
 	echo "Please ensure your system is upto date and rebooted if needed";
@@ -42,14 +50,37 @@ done
 #echo "test"; #TEST
 
 # Download and install nessasary repos
-printf "Setting up nessasary repositories\t";
-yum install wget -y &> /dev/null;
-wget https://repos.fedorapeople.org/repos/pulp/pulp/`echo $REPO`-pulp.repo -O /etc/yum.repos.d/`echo $REPO`-pulp.repo &> /dev/null;
-sudo yum install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm &> /dev/null;
-printf "[DONE]";
+printf " * [FAKE]Setting up nessasary repositories\t";
+#yum install wget -y &> /dev/null;
+#wget https://repos.fedorapeople.org/repos/pulp/pulp/`echo $REPO`-pulp.repo -O /etc/yum.repos.d/`echo $REPO`-pulp.repo &> /dev/null;
+#yum install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm &> /dev/null;
+printf " $DONE ";
 echo;
 
-#Install pulp admin client
-printf "";
+# Install pulp admin client
+printf " * [Fake]Installing pulp-admin client\t\t";
+#yum groupinstall pulp-admini &> /dev/null
+#inject config changes into /etc/pulp/admin/admin.conf
+printf " $DONE ";
+echo;
 
+# Setup consumer and agent
+printf " * [FAKE]Installing pulp consumer and agent\t";
+#yum groupinstall pulp-consumer-qpid &> /dev/null
+#Inject config changes into /etc/pulp/consumer/consumer.conf
+printf " $DONE ";
+echo;
 
+# Start consumer services
+printf " * [FAKE]Getting things started\t\t\t";
+#systemctl enable goferd &> /dev/null
+#systemctl start goferd &> /dev/null
+printf " $DONE ";
+echo;
+
+# Setup docker components for pulp-admin
+printf " * [FAKE]Installing pulp-admin docker components\t";
+
+echo "Setup completed";echo;
+
+echo ""
