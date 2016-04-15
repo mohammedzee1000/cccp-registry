7bd5981a2fd7c5967497b0f1021164bed0bed0ef
Mohammed Zeeshan Ahmed <moahmed@dhcp35-90.lab.eng.blr.redhat.com>
2016-04-15 20:16:20 +0530

    Added ability to accept an orderfile.

--- scripts/builddockerfiles.sh
+++ scripts/builddockerfiles.sh
@@ -10,14 +10,15 @@
 
 DIRROOT=$1;
 PROJECT=$2;
-LOGFILE=$3
+ORDERFILE=$3;
+LOGFILE="$HOME/builddockerfile";
 
 function usage() {
-	echo "USAGE : $0 [DIRROOT] [PROJECTNAME] [LOGFILEPATH].";
+	echo "USAGE : $0 [DIRROOT] [PROJECTNAME] [BASEDIR] [ORDERFILE]";
 	echo;
 	echo "*   Directory Root - The folder which containes dockerfiles, remember only one Dockerfile per directory/subdirectory.";
 	echo "*   PROJECTNAME - Name you wish to assign to the project.";
-	echo "*   The logfile where you wish to record the build details, defaults to $HOME/builddockerfiles";
+	echo "    ORDERFILE - Pattern ";
 	echo;
 }
 
@@ -39,50 +40,78 @@ function err() {
 function build() {
 	level=$1;
 	local ITEM;
-	for ITEM in `ls`; do
-		#echo $ITEM; #test
-		if [ -d "./$ITEM" -a ! -L "./$ITEM" ]; then
-			#echo "$ITEM is directory" #test
-			#ls -l $ITEM; #test
-			pushd ./$ITEM &> /dev/null;
-			build $level;
+	if [ -f $ORDERFILE]; then
 
-			ls | grep -i Dockerfile &> /dev/null;
-			
-			if [ $? -eq 0 ]; then
-
-				echo;echo;
-				echo "* Found Dockerfile at $PWD";
-				buildid_t="$PROJECT/$ITEM";
-				buildid=`echo $buildid_t | tr '[:upper:]' '[:lower:]'`;
-				printf "** Building as $buildid, this could take a while ...";
-				printf "\n\n\nBuild $PWD dockerfile\n" >> $LOGFILE; 
-				docker build -t $buildid . >> $LOGFILE 2>&1;
+		for ITEM in `echo $ORDERFILE`; do
+			if [ -d "$ITEM" -a ! -L "$ITEM" ]; then
+				ls ./$ITEM | grep -i Dockerfile &> /dev/null;
 				if [ $? -eq 0 ]; then
-					state="success";
-					docker rmi $buildid &> /dev/null;
-				else
-					state="failure";
+					echo;echo;
+					echo "* Found dockerfile at $PWD/ITEM...";
+					buildid_t="$PROJECT/$ITEM";
+					buildid=`echo $buildid_t | tr '[:upper:]' '[:lower:]'`;
+					printf "** Building as $buildid...\n";
+					docker build -t $buildid . >> $LOGFILE 2>&1;
+               		                if [ $? -eq 0 ]; then
+                	               		 state="success";
+                        	                 docker rmi $buildid &> /dev/null;
+                        	        else
+                                	         state="failure";
+                               		fi
+					printf "$state\n\n";
+					printf "$state\n\n" >> $LOGFILE;
+
 				fi
 				echo;echo;
-				printf "$state\n\n" >> $LOGFILE;
-				printf "$state\n\n";
-			fi	
-			popd &> /dev/null;
-		fi
-	done	
+
+			fi
+		done
+
+	else
+
+		for ITEM in `ls`; do
+			#echo $ITEM; #test
+			if [ -d "./$ITEM" -a ! -L "./$ITEM" ]; then
+				#echo "$ITEM is directory" #test
+				#ls -l $ITEM; #test
+				pushd ./$ITEM &> /dev/null;
+				build $level;
+
+				ls | grep -i Dockerfile &> /dev/null;
+			
+				if [ $? -eq 0 ]; then
+
+					echo;echo;
+					echo "* Found Dockerfile at $PWD";
+					buildid_t="$PROJECT/$ITEM";
+					buildid=`echo $buildid_t | tr '[:upper:]' '[:lower:]'`;
+					printf "** Building as $buildid, this could take a while ...";
+					printf "\n\n\nBuild $PWD dockerfile\n" >> $LOGFILE; 
+					docker build -t $buildid . >> $LOGFILE 2>&1;
+					if [ $? -eq 0 ]; then
+						state="success";
+						docker rmi $buildid &> /dev/null;
+					else
+						state="failure";
+					fi
+					echo;echo;
+					printf "$state\n\n" >> $LOGFILE;
+					printf "$state\n\n";
+				fi	
+				popd &> /dev/null;
+			fi
+		done
+
+	fi	
 }
 
 if [ $# -lt 2 ]; then
 	err INVAL_USAGE;
 fi
 
-if [ -z $LOGFILE ]; then
 
-	LOGFILE="$HOME/builddockerfile";
-	echo "Log file not provided, defaulting to $LOGFILE";
+echo "Log file not provided, defaulting to $LOGFILE";
 
-fi
 
 if [ -f $LOGFILE ]; then
 	rm -rf $LOGFILE;
