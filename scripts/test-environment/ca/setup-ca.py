a84d6aed26f4f20704392af5c27df641b1d9a371
mohammedzee1000 <mohammed.zee1000@gmail.com>
2016-05-12 20:18:29 +0530

    Basic setp-ca script.

---
+++ scripts/test-environment/ca/setup-ca.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+# imports
+import ConfigParser
+
+# Base variables
+CA_LOC = "/root"
+
+# Consts
+CA_CERT = "ca.cert.pem"
+CA_KEY = "ca.key.pem"
+
+# Derived
+CA_DIR = CA_LOC + "/ca"
+INT_DIR = CA_DIR + "/intermediate"
+
