c16bef6b880e8f762339401331fab658c047dce0
mohammedzee1000 <mohammed.zee1000@gmail.com>
2016-05-23 20:56:06 +0530

    Updated the config, added functionality and cleaned code.
    Updated the config file to its latest version.
    Added ability to modify certificate entries to config manager.
    Cleaned up the code and added more comments.

--- scripts/atomic-registry/setup/atomic_registry_setup.py
+++ scripts/atomic-registry/setup/atomic_registry_setup.py
@@ -1,12 +1,20 @@
 #!/bin/python
 
 import yaml
+import json
 import os
 import sys
 from subprocess import call
 from enum import Enum
 import re
+from itertools import chain
+from collections import Set, Sequence
 
+class quoted(str): pass
+
+def quoted_presenter(dumper, data):
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
+yaml.add_representer(quoted, quoted_presenter)
 
 # Globals Variables :
 class InpMode(Enum):
@@ -30,40 +38,68 @@ class AtomicRegistryConfigManager:
 
     # All the configurable sections functions are located here.
 
-    # * Certs
-    def add_named_certs(self, certfile = None, keyfile = None):
+    # * Certs - All functions that handle certificate related configs
 
-        thesection = self.config["assetConfig"]["servingInfo"]["namedCertificates"]
+    def add_named_certs(self, certfile = None, keyfile = None, names = None):
+        """Function adds a named certificate to the config file."""
 
-        print thesection
+        # Get what is already present in the config section to modify
+        currcertsconfig = self.config["assetConfig"]["servingInfo"]["namedCertificates"]
+
+        print "Appending entry for named cert " + certfile + " and " + keyfile + "..."
+
+        # Add the cert file and key file to the content to be added to the section
+        content = {"keyFile": keyfile, "certFile": certfile}
+
+        # If any names list has been passed
+        if names != None:
+            nmlist = []
+
+            # Go through the list and make every item quoted, so yaml dumper can handle it appropriately.
+            for item in names:
+                nmlist.append(quoted(item))
+
+            # Add the name list to the content to be written as 'names'
+            content["names"] = nmlist
+
+        toadd = None
+
+        # If current section is emply, then set current content to list, otherwise, append it to existing certs config
+        if currcertsconfig == None:
+            toadd = [content]
+
+        else:
+            temp = currcertsconfig
+            toadd = temp + [content]
+
+        # Update the section with appropriate changes.
+        self.config["assetConfig"]["servingInfo"]["namedCertificates"] = toadd
 
         return
 
-
     # Section Ends
 
-
-    # Test functions
+    # * Test functions - Handles testing of the functionality of code
     def test_print_config(self):
-        """Test function"""
+        """Test function - Print current config on screen"""
 
         print self.config
 
         return
 
-
     def test_finalize_config(self):
+        """Test the finalize config on a test yaml output file."""
 
         with open(self.oc_test_config, "w") as yamlfile:
-            yamlfile.write(yaml.dump(self.config))
-
+                yamlfile.write(yaml.dump(self.config,default_flow_style=False))
         return
 
     # Finalize Function
     def finalize_config(self):
+        """Writes the config back to config file, making it permanent"""
 
         with open(self.oc_master_config, "w") as ymlfile:
-            ymlfile.write(yaml.dump(self.config))
+            ymlfile.write(yaml.dump(self.config, default_flow_style=False))
 
         return
 
@@ -103,17 +139,18 @@ class AtomicRegistryQuickstartSetup:
         return
 
     def customize(self):
-        """Applying configuration changes to the atomic registry"""
+        """Applying configuration changes to the atomic registry - based on user input"""
 
         self.config_manager = AtomicRegistryConfigManager()
-
 
         return
 
     def test_customize(self):
-        """Applying configuration changes to the atomic registry test"""
+        """Applying configuration changes to the atomic registry - testing"""
 
         self.config_manager = AtomicRegistryConfigManager()
+        self.config_manager.add_named_certs("test.crt", "test.key")
+        self.config_manager.add_named_certs("test1.crt", "test1.key", ["google.com", "test.com"])
 
         #self.config_manager.test_print_config()  # Test
         self.config_manager.test_finalize_config() # Test
