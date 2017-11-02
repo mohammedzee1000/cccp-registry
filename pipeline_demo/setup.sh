63deb31c7e6f51d7e451380834f50c010be09276
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2017-11-02 12:10:04 +0530

    Modified script to allow cleanup of vms.

--- pipeline_demo/setup.sh
+++ pipeline_demo/setup.sh
@@ -8,30 +8,35 @@ VAGRANT_USER="root";
 VAGRANT_IP="";
 
 setup_node(){
-	NODE=${1};
+	ACTION=${1};
+	NODE=${2};
 	pushd ${NODE};
 	vagrant destroy;
-	vagrant up;
-	VAGRANT_IP="`vagrant ssh-config | grep HostName | cut -d ' ' -f4`";
-	sshpass -proot ssh-copy-id -o StrictHostKeyChecking=no ${VAGRANT_USER}@${VAGRANT_IP};
+	if [ $ACTION == "create" ]; then
+		vagrant up;
+		VAGRANT_IP="`vagrant ssh-config | grep HostName | cut -d ' ' -f4`";
+		sshpass -proot ssh-copy-id -o StrictHostKeyChecking=no ${VAGRANT_USER}@${VAGRANT_IP};
+	fi
 	popd;
 }
 
+ACTION=${1:-"create"}
+
 # Setup the nodes and gather their ips
 CURRENT_NODE="jenkins_master";
-setup_node ${CURRENT_NODE};
+setup_node ${ACTION} ${CURRENT_NODE};
 JENKINS_MASTER=${VAGRANT_IP};
 
 CURRENT_NODE="jenkins_slave";
-setup_node ${CURRENT_NODE};
+setup_node ${ACTION} ${CURRENT_NODE};
 JENKINS_SLAVE=${VAGRANT_IP};
 
 CURRENT_NODE="openshift";
-setup_node ${CURRENT_NODE};
+setup_node ${ACTION} ${CURRENT_NODE};
 OPENSHIFT=${VAGRANT_IP};
 
 CURRENT_NODE="scanner";
-setup_node ${CURRENT_NODE};
+setup_node ${ACTION} ${CURRENT_NODE};
 SCANNER=${VAGRANT_IP};
 # Generate pipeline inventory file
 cat >${hosts_file} <<EOF
