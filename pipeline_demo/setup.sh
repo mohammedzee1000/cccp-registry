eb1f2bdcb1dde66b086ac9493397ad6e722b3888
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2017-12-01 16:33:00 +0530

    Updating env script.

--- pipeline_demo/setup.sh
+++ pipeline_demo/setup.sh
@@ -39,37 +39,4 @@ CURRENT_NODE="scanner";
 setup_node ${ACTION} ${CURRENT_NODE};
 SCANNER=${VAGRANT_IP};
 # Generate pipeline inventory file
-cat >${hosts_file} <<EOF
-[all:children]
-jenkins_master
-jenkins_slaves
-openshift
-scanner_worker
-
-[jenkins_master]
-${JENKINS_MASTER}
-
-[jenkins_slaves]
-${JENKINS_SLAVE}
-
-[openshift]
-${OPENSHIFT}
-
-[scanner_worker]
-${SCANNER}
-
-[all:vars]
-db_host=${JENKINS_MASTER}
-cccp_index_repo=https://github.com/mohammedzee1000/test_index.git
-beanstalk_server=${OPENSHIFT}
-copy_ssl_certs=True
-oc_slave=${JENKINS_SLAVE}
-jenkins_public_key_file=/home/moahmed/cccp-jenkins1.pub
-jenkins_private_key_file=/home/moahmed/cccp-jenkins1
-public_registry=${JENKINS_SLAVE}
-test=True
-intranet_registry=${JENKINS_SLAVE}:5000
-setup_nfs=True
-test_nfs_share=${SCANNER}:/nfsshare
-deployment=test
-EOF
+sh ./gen_inventory.sh ${hosts_file} ${JENKINS_MASTER} ${JENKINS_SLAVE} ${OPENSHIFT} ${SCANNER}
